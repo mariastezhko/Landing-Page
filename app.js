4e673bc41917f6b009008231f85b84827e2ce148
Maria Stezhko <stezhko@gmail.com>
2018-02-07 11:55:35 -0800

    Add view files

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var http = require('http');
 var fs = require('fs');
 // Create a server using http module:
 var server = http.createServer(function (request, response){
-    console.logcopy('client request URL: ', request.url);
+    console.log('client request URL: ', request.url);
     if(request.url === '/') {
         fs.readFile('index.html', 'utf8', function (errors, contents){
             response.writeHead(200, {'Content-Type': 'text/html'});
