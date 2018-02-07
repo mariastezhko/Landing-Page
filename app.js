d9293a521aaffb5850f57c72805c4e8eb5c33d14
Maria Stezhko <stezhko@gmail.com>
2018-02-07 11:42:06 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+// Get the http module:
+var http = require('http');
+// Get the fs module (for reading and writing content for responses)
+var fs = require('fs');
+// Create a server using http module:
+var server = http.createServer(function (request, response){
+    console.logcopy('client request URL: ', request.url);
+    if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents);
+            response.end();
+        });
+    }
+    else if (request.url === "/ninjas") {
+         fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents);
+             response.end();
+         });
+    }
+    else if (request.url === "/dojos/new") {
+         fs.readFile('dojos.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents);
+             response.end();
+         });
+    }
+    else {
+        response.end('File not found!!!');
+    }
+});
+server.listen(6789);
+console.log("Running in localhost at port 6789");
